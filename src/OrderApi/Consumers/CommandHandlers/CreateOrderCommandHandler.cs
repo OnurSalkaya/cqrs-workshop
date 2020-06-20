cf4d41a92119de99b295ba26a150837ef177e6b7
Onur SALKAYA <onursalkaya@gmail.com>
2020-06-20 17:14:33 +0300

    code fix

--- src/OrderApi/Consumers/CommandHandlers/CreateOrderCommandHandler.cs
+++ src/OrderApi/Consumers/CommandHandlers/CreateOrderCommandHandler.cs
@@ -1,11 +1,7 @@
 ﻿using MassTransit;
-using OrderApi.Domain.Commands;
-using OrderApi.Domain.Events;
 using OrderApi.Data.Entities;
 using OrderApi.Data.Repositories.MsSql;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using OrderApi.Domain.Commands;
 using System.Threading.Tasks;
 
 namespace OrderApi.Consumers.CommandHandlers
@@ -22,7 +18,7 @@ namespace OrderApi.Consumers.CommandHandlers
         public async Task Consume(ConsumeContext<CreateOrderCommand> context)
         {
             var order = new Order(context.Message.Id, context.Message.OrderCode, context.Message.OrderDate, context.Message.UserId, context.Message.TotalPrice);
-
+                
             await _orderRepository.Create(order);
         }
     }
